868de922fb7f4dd228e791ecbfbd076fbde08792
DimWorks <dimulatoraz@gmail.com>
2023-11-05 16:36:43 +0700

    created make

--- PR-1/adder.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void add_queue(char* path, char* data, char* struc);
-void add_ht(char* path, char* data, char* struc);
-void add_set(char* path, char* data, char* struc);
--- PR-1/file.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void read_file(char*, char*);
-void write_to_file(char*, char*, char*);
