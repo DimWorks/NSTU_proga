486e1cda40bae20e719ed962e80e8dba16dfc9a9
Dmitriy <dimulatoraz@gmail.com>
2023-11-07 22:03:38 +0700

    Done

--- PR-1/main.c
+++ PR-1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 
@@ -16,7 +17,7 @@ void call_dataBase(char* command, char* data, char* key)
 
 		break;
 	case 0:
-		char* path = "/home/dima/NSTU_proga/PR-1/dataBase.out\0";
+		char* path = "/home/dmitriy/NSTU_proga/PR-1/dataBase.out\0";
 		execl(path, command, data, key, NULL);
 		//return;
 
@@ -32,13 +33,84 @@ int main()
 	printf("Enter the command: ");
 	scanf("%s", command);
 
-	printf("Enter the data: ");
 	char data[_SIZE_];
-	scanf("%s", data);
-
-	printf("Enter the key: ");
 	char key[_SIZE_];
-	scanf("%s", key);
+
+	//printf("Enter the data: ");
+	//scanf("%s", data);
+
+	//printf("Enter the key: ");
+	//scanf("%s", key);
+
+	if (strcmp(command, "SADD") == 0)
+		{
+			printf("Enter the data: ");
+			scanf("%s", data);
+			strcpy(key, "key");
+		}
+		else if (strcmp(command, "SPUSH") == 0)
+		{
+			printf("Enter the data: ");
+			scanf("%s", data);
+			strcpy(key, "key");
+		}
+		else if (strcmp(command, "QPUSH") == 0)
+		{
+			printf("Enter the data: ");
+			scanf("%s", data);
+			strcpy(key, "key");
+		}
+		else if (strcmp(command, "HSET") == 0)
+		{
+			printf("Enter the data: ");
+			scanf("%s", data);
+
+			printf("Enter the key: ");
+			scanf("%s", key);
+
+		}
+		//------------REMOVE------------
+
+		else if (strcmp(command, "SREM") == 0)
+		{
+			printf("Enter the key: ");
+			scanf("%s", key);
+			strcpy(data, "data");
+		}
+		else if (strcmp(command, "SPOP") == 0)
+		{
+			//printf("%s\n", pop_stack());
+			strcpy(data, "data");
+			strcpy(key, "key");
+		}
+		else if (strcmp(command, "QPOP") == 0)
+		{
+			//printf("%s\n", pop_queue());
+			strcpy(data, "data");
+			strcpy(key, "key");
+		}
+		else if (strcmp(command, "HDEL") == 0)
+		{
+			printf("Enter the key: ");
+			scanf("%s", key);
+			strcpy(data, "data");
+		}
+
+		//------------READ------------
+		else if (strcmp(command, "SISMEMBER") == 0)
+		{
+			printf("Why?\n");
+		}
+		else if (strcmp(command, "HGET") == 0)
+		{
+			printf("Enter the key: ");
+			scanf("%s", key);
+
+		}
+		else
+		{
+			printf("ERROR: incorrect command\n");
+		}
 
 	call_dataBase(command, data, key);
 	
